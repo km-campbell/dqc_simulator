72726ad2878dbf9516d4f9bcbf686ea099fb41dc
Kenny <kennycampbell1@btinternet.com>
2024-01-23 11:00:44 +0000

    started converting replot_diff_between_first_order_and_exact to show a normalised result

--- src/dqc_simulator/util/first_order_approx.py
+++ src/dqc_simulator/util/first_order_approx.py
@@ -26,7 +26,34 @@ def f_out_with_ent_error_first_order(num_epr_pairs, F_werner):
         
     """
     error = 1 - F_werner
-    f_out = 1 - num_epr_pairs * error
+    f_out = 1 - num_epr_pairs * error 
+    #is it also worth considering  (1 - error)**num_epr_pairs which fits the data better?
+    return f_out
+
+def f_out_with_exponentially_growing_ent_error(num_epr_pairs, F_werner):
+    """
+    A simple approximation to the error caused by the distribution of 
+    entangled pairs often made in the literature. Here, the output fidelity 
+    is assumed to decrease exponentially with the number of EPR pairs but
+    the exponential is not fitted to any numerical or empirical data.
+
+    Parameters
+    ----------
+    num_epr_pairs : int
+        The number of entangled pairs in the circuit. They would be EPR pairs
+        ideally but in reality the Werner state is distributed
+    F_werner : float
+        The Werner state fidelity of the distributed imperfect entangled pairs 
+        (assumed to be in the Werner state).
+
+    Returns
+    -------
+    f_out : float
+        The fidelity at the output of the circuit.
+        
+    """
+    error = 1 - F_werner
+    f_out = (1 - error)**num_epr_pairs
     return f_out
 
 def f_out_with_cnot_depol_first_order(num_cnots, p_error):
@@ -49,6 +76,32 @@ def f_out_with_cnot_depol_first_order(num_cnots, p_error):
         
     """
     f_out = 1 - num_cnots * p_error
+    #is it also worth considering:
+    #f_out = (1 - p_error)**num_cnots
+    return f_out
+
+def f_out_with_exponentially_growing_cnot_depol(num_cnots, p_error):
+    """
+    A common approximation to the error caused by depolarisation due to
+    imperfect local gates in the literature. Here, it is assumed the gate error
+    grows exponentially with the number of CNOT gates but no fitting is done
+    to simulated or empirical data to give a better fit.
+
+    Parameters
+    ----------
+    num_cnots : int
+        The number of local CNOT gates in the circuit.
+    p_error : float
+        The probability of depolarisation taking place after each 
+        local CNOT gate.
+
+    Returns
+    -------
+    f_out : float
+        The fidelity at the output of the circuit.
+        
+    """
+    f_out = (1 - p_error)**num_cnots
     return f_out
 
 def f_out_with_mem_depol_first_order(depolar_rate, num_single_qubit_gates, 
@@ -113,4 +166,9 @@ def f_out_with_mem_depol_first_order(depolar_rate, num_single_qubit_gates,
                        num_epr_pairs * ent_distr_time + 
                        num_classical_comms * node_distance/(200e6))
     f_out = 1 - depolar_rate * circuit_runtime
+    #is it also worth considering:
+    #f_out = (1-depolar_rate)**circuit_runtime
     return f_out
+
+
+    
