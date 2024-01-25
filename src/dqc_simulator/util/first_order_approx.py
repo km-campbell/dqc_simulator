ddbae65748886fc7d4decd91bb248c1101e2677e
Kenny <kennycampbell1@btinternet.com>
2024-01-25 14:52:31 +0000

    Converted unwisely normalised plots of difference between simulated and approx results back to percentage difference for single remote gates

--- src/dqc_simulator/util/first_order_approx.py
+++ src/dqc_simulator/util/first_order_approx.py
@@ -170,5 +170,68 @@ def f_out_with_mem_depol_first_order(depolar_rate, num_single_qubit_gates,
     #f_out = (1-depolar_rate)**circuit_runtime
     return f_out
 
+def f_out_with_exponentially_growing_mem_depol(
+                                     depolar_rate, num_single_qubit_gates, 
+                                     num_cnots, num_measurements,
+                                     num_epr_pairs, num_classical_comms,
+                                     single_qubit_gate_time=135e3,
+                                     cnot_gate_time=600e3, 
+                                     measurement_time=300e3, 
+                                     ent_distr_time=1/182,
+                                     node_distance=2):
+    """
+    The first-order approximation to the error caused by time-dependent memory
+    depolarisation.
+    
+    Parameters
+    ----------
+    depolar_rate : float
+        Memory depolarisation rate in Hz.
+    num_single_qubit_gates : int
+        The number of single-qubit gates in the circuit.
+    num_cnots : int
+        The number of local CNOT gates in the circuit.
+    num_epr_pairs : int
+        The number of EPR pairs distributed between nodes.
+    num_measurements : int
+        The number of measurements conducted in the circuit.
+    num_classical_comms : int
+        The number of classical messages sent between nodes
+    single_qubit_gate_time : float
+        Time taken to complete single-qubit gate in s. I assume here that 
+        all single-qubit gates take the same amount of time.
+    cnot_gate_time : float
+        Time taken to complete CNOT gate in s.
+    measurement_time : float
+        Time taken to measure the state of a single qubit in the z-basis.
+    ent_distr_time : float
+        The time taken to distribute entanglement (EPR pairs) between two
+        nodes. I assume this is the same between all nodes.
+    node_distance : float, optional
+        The distance between nodes in m. I assume this is the same between all
+        nodes. The default is 2m.
 
+    Returns
+    -------
+    f_out : float
+        The fidelity at the output of the circuit.
+
+    """
+    #NOTE: for the following runtime calculation I make the simplifying
+    #assumption that the measurement dependent gates all occur (the worst case
+    #scenario for latency) and that the only delay in classical communication 
+    #is the time taken to travel through the fibre (there is no processing time
+    #to recognise that a measurement has occurred and should be sent). I think
+    #that both of these assumptions should have a relatively negligible effect
+    #relative to the entanglement distillation time and two-qubit gate time,
+    #which dominate. All times are in s and speeds in m/s
+    c_optical_fibre = 200e6 #light propatation speed through optical fibre in
+                            #m/s
+    circuit_runtime = (num_single_qubit_gates * single_qubit_gate_time + 
+                       num_cnots * cnot_gate_time +
+                       num_measurements * measurement_time + 
+                       num_epr_pairs * ent_distr_time + 
+                       num_classical_comms * node_distance/(200e6))
+    f_out = (1-depolar_rate)**circuit_runtime
+    return f_out
     
