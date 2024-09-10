52d879358f176dfcd8c536ff111dd9ee1dacdc8d
Kenny <kennycampbell1@btinternet.com>
2024-09-10 13:27:27 +0100

    Added connections model

---
+++ src/dqc_simulator/hardware/connections.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# # -*- coding: utf-8 -*-
+# 
+# Created on Tue Sep 10 14:06:26 2024
+# 
+# @author: kenny
+# 
+# =============================================================================
+"""
+An extensible library of `netsquid.nodes.connections.Connection <connections>`
+ useful for a DQC.
+"""
