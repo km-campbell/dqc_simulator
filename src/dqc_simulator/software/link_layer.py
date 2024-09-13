32a077f0368103cee4ff1a386ec2b3fc7dd63c2f
Kenny <kennycampbell1@btinternet.com>
2024-09-13 11:09:19 +0100

    New modules added for link_layer and physical_layer of Wehner stack

---
+++ src/dqc_simulator/software/link_layer.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# =============================================================================
+# Created on Fri Sep 13 09:28:30 2024
+# 
+# @author: kenny
+# =============================================================================
+
+"""
+Protocols for the link layer similar to that defined in the Wehner network stack.
+
+Notes
+-----
+The original [1]_ and revised [2]_ papers on the Wehner network stack give 
+further details.
+
+References
+----------
+.. [1] A. Dahlberg, et al., A link layer protocol for quantum networks, in: 
+       Proceedings of the ACM Special Interest Group on Data Communication,
+       ACM (2019)
+
+.. [2] M. Pompili, et al., Experimental demonstration of entanglement delivery 
+       using a quantum network stack, npj Quantum Information, 8 (2022)
+
+"""
+
